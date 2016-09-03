8c4889440ec8739409101cdeedf26e1baa023451
Bharath <bharath2k2in@gmail.com>
2016-09-04 00:07:41 +0200

    Initial commit for hackathon

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartAdviceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the smartAdviceApp
+ */
+angular.module('smartAdviceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartAdviceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smartAdviceApp
+ */
+angular.module('smartAdviceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
